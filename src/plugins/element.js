ccb2ad7e6b2289234026efc6b5ce6fc74c0f57b3
gan_xingba <1063010784@qq.com>
2021-01-11 16:56:39 +0800

    'curd'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Dialog } from 'element-ui'
+// import {Dialog,Form,Button} from 'element-ui'
+import ElementUI from 'element-ui';
 
-Vue.use(Button)
-Vue.use(Dialog)
+// Vue.use(Button)
+// Vue.use(Dialog)
+// Vue.use(CollapseTransition)
+Vue.use(ElementUI)
